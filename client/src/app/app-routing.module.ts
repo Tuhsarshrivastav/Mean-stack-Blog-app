759af5d52e0e70a8768b36056c3b355b82baa380
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 <ts3657@gmail.com>
2021-11-23 15:14:59 +0530

    added routing for all modulee

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component'
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/articles/articles.module.ts
+++ client/src/app/articles/articles.module.ts
@@ -5,7 +5,7 @@ import { ArticleDetailsComponent } from './article-details/article-details.compo
 import { LatestArticlesComponent } from './latest-articles/latest-articles.component';
 import { FeaturedArticlesComponent } from './featured-articles/featured-articles.component';
 import { CategoriesComponent } from './categories/categories.component';
-
+import { ArticlesRoutingModule } from '../articles/articles-routing.module'
 
 
 @NgModule({
@@ -17,7 +17,8 @@ import { CategoriesComponent } from './categories/categories.component';
     CategoriesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ArticlesRoutingModule
   ],
   exports: [LatestArticlesComponent]
 })
