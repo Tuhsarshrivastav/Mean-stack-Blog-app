a1bd1ac8939dfe7b7773c9833a93e7830d93f63b
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 <ts3657@gmail.com>
2021-11-25 13:11:33 +0530

    added register api

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,24 @@ const userModel = require("../models/user");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  const userDetails = new userModel({
-    name: "Vikas",
-    email: "vikas@gmail.com",
-    password: "vikas@123",
+router.post("/register", function (req, res, next) {
+  var userDetails = new userModel({
+    name: req.body.Name,
+    email: req.body.Email,
+    password: req.body.Password,
   });
 
-  userDetails.save(function (err, req1) {
-    if (err) throw err;
-
-    res.render("index", { title: "User Data Inserted" });
-  });
+  userDetails
+    .save()
+    .then((doc) => {
+      res.status(201).json({
+        message: "Inserted Successfully",
+        results: doc,
+      });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
